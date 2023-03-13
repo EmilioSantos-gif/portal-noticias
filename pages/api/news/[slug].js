464c1f37bcb77c208f587d4fd9f20c7a703e7fc0
Emilio Santos <emilioariel1018@gmail.com>
2023-03-13 10:36:15 -0400

    Completando pagina de noticia individual 1:49:00

--- pages/api/news/[slug].js
+++ pages/api/news/[slug].js
@@ -1,5 +1,12 @@
 const { news } = require("./data.json");
 
 export default (req, res) => {
-    res.status(200).json({name: 'Json Doeee'})
+    
+    if (req.method === "GET") {
+        const newsArticle = news.filter((item) => item.slug == req.query.slug);
+        res.status(200).json(newsArticle);
+    } else {
+        res.setHeader("Allow", ["GET"]);
+        res.status(405).json({message: `Method ${req.method} is not supported`})
+    }
 }
--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -1,13 +1,39 @@
 import { useRouter } from "next/router"
 import Layout from "@/components/Layout";
+import Image from "next/image"
+import { API_URL } from '@/config/index'
+import Link from "next/link";
+import styles from "@/styles/News.module.css"
 
-export default function SingleNews() {
+export default function SingleNews({ news }) {
     const router = useRouter();
     console.log("router====>", router);
   return (
     <Layout>
-      <h2>Noticia unica</h2>
-      <h3>{router.query.slug}</h3>
+        <div className={styles.news}>
+            <span>{news.date} {news.time}</span>
+            <h1>{news.name}</h1>
+            {news.image && (
+                <div className={styles.image}>
+                    <Image src={news.image} width={900} height={600} />
+                </div>
+            )}
+            <p>{news.detail}</p>
+            <Link legacyBehavior href="/news">
+                <a className={styles.back}>Volver atrás</a>
+            </Link>
+        </div>
     </Layout>
   );
+}
+
+
+export async function getServerSideProps({ query: { slug }}){
+    const res = await fetch(`${API_URL}/api/news/${slug}`);
+    const singleNews = await res.json();
+    return {
+        props: {
+            news: singleNews[0],
+        },
+    };
 }
